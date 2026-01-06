14c262b13c243b16527e664505ae81c439347cd4
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-01-06 12:13:07 +0530

    Refactor landing page components: update HomePage, Awards, Education, Hero, Pricing, and Stats with new layouts and content

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Hero() {
     return (
-        <h1>Hero Section</h1>
+        <div className='container p-5 text-center mb-5'>
+            <div className='row'>
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
+            </div>
+
+            <h1 className='mt-5'>Inves in everything</h1>
+            <p>Onilne platform to invest in stocks, derivatives, mutual funds, and more</p>
+            <button  className='p-2 btn btn-primary mb-5' style={{width:"20%" , margin: "0 auto" }}>Signup now</button>
+        </div>
     );
 }
 

88a80813ce6a65613913c837e98125840417bd3d
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-01-06 12:31:13 +0530

    Add full Zerodha Clone project (frontend + backend)

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomePage from './landing_page/home/HomePage';
+import ProductPage from './landing_page/product/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import AboutPage from './landing_page/about/AboutPage';
+import  SupportPage from './landing_page/support/SupportPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+    < HomePage />
+
+
+  </React.StrictMode>
+);
+
---
+++ frontend/src/landing_page/product/Universe.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Universe() {
+    return (<h1>Universe Component</h1>);
+}
+
+export default Universe;

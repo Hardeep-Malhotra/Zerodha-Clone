5b3b498cc96f7e8a0430a7da4f21d2d6e07ff85f
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-01-09 12:07:18 +0530

    Build Complete Support component

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,7 +1,113 @@
-import React from 'react'
+import React from "react";
 
 function CreateTicket() {
-    return (<h1>CreateTicket component </h1>);
+  const iconStyle = {
+    fontSize: "20px",
+    marginRight: "8px",
+  };
+
+  const linkStyle = {
+    textDecoration: "none",
+    lineHeight: "2.5rem",
+    fontSize: "18px",
+    display: "block",
+  };
+
+  return (
+    <div className="container">
+      {/* ROW 1 */}
+      <div className="row">
+        <h1 fs-2 style={{ marginLeft: "30px" }}> To create a ticket, select a relevent topic </h1>
+
+        <div className="col-4 p-5 mt-3">
+          <h4 className="mb-5">
+            <i className="fa fa-plus-circle" style={iconStyle}></i>
+            Account Opening
+          </h4>
+
+          <a href="#" style={linkStyle}>Online Account Opening</a>
+          <a href="#" style={linkStyle}>Offline Account Opening</a>
+          <a href="#" style={linkStyle}>Company, Partnership and HUF Account</a>
+          <a href="#" style={linkStyle}>Opening</a>
+          <a href="#" style={linkStyle}>NRI Account Opening</a>
+          <a href="#" style={linkStyle}>Charges at Zerodha</a>
+          <a href="#" style={linkStyle}>Getting Started</a>
+        </div>
+
+        <div className="col-4 p-5 mt-3">
+          <h4 className="mb-5">
+            <i className="fa fa-user" style={iconStyle}></i>
+            Your Zerodha Account
+          </h4>
+
+          <a href="#" style={linkStyle}>Login Credentials</a>
+          <a href="#" style={linkStyle}>Account Modification and Segment Addition</a>
+          <a href="#" style={linkStyle}>DP ID and bank details</a>
+          <a href="#" style={linkStyle}>Your Profile</a>
+          <a href="#" style={linkStyle}>Transfer and conversion of shares</a>
+        </div>
+
+        <div className="col-4 p-5 mt-3">
+          <h4 className="mb-5">
+            <i className="fa fa-bar-chart" style={iconStyle}></i>
+            Trading & Markets
+          </h4>
+
+          <a href="#" style={linkStyle}>Margin / Leverage</a>
+          <a href="#" style={linkStyle}>Kite Web and Mobile</a>
+          <a href="#" style={linkStyle}>Trading FAQs</a>
+          <a href="#" style={linkStyle}>Corporate Actions</a>
+          <a href="#" style={linkStyle}>Sentinel</a>
+          <a href="#" style={linkStyle}>Kite API</a>
+          <a href="#" style={linkStyle}>Pi and other platforms</a>
+          <a href="#" style={linkStyle}>Stockreports+</a>
+          <a href="#" style={linkStyle}>GTT</a>
+        </div>
+      </div>
+
+      {/* ROW 2 */}
+      <div className="row">
+        <div className="col-4 p-5">
+          <h4 className="mb-5">
+            <i className="fa fa-credit-card" style={iconStyle}></i>
+            Funds
+          </h4>
+
+          <a href="#" style={linkStyle}>Add money</a>
+          <a href="#" style={linkStyle}>Withdraw money</a>
+          <a href="#" style={linkStyle}>Add bank accounts</a>
+          <a href="#" style={linkStyle}>eMandates</a>
+        </div>
+
+        <div className="col-4 p-5">
+          <h4 className="mb-5">
+            <i className="fa fa-desktop" style={iconStyle}></i>
+            Console
+          </h4>
+
+          <a href="#" style={linkStyle}>Portfolio</a>
+          <a href="#" style={linkStyle}>Corporate actions</a>
+          <a href="#" style={linkStyle}>Funds statement</a>
+          <a href="#" style={linkStyle}>Reports</a>
+          <a href="#" style={linkStyle}>Profile</a>
+          <a href="#" style={linkStyle}>Segment</a>
+        </div>
+
+        <div className="col-4 p-5">
+          <h4 className="mb-5">
+            <i className="fa fa-circle-thin" style={iconStyle}></i>
+            Coin
+          </h4>
+
+          <a href="#" style={linkStyle}>Mutual funds</a>
+          <a href="#" style={linkStyle}>National Pension Scheme (NPS)</a>
+          <a href="#" style={linkStyle}>Features on Coin</a>
+          <a href="#" style={linkStyle}>Payments on Coin</a>
+          <a href="#" style={linkStyle}>General</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero Component  </h1>
-     );
+  return (
+    <section className="container-fluid" id="supportHero">
+      
+      <div className="supportTop">
+        <h4 className="supporPort">Support Portal</h4>
+        <a href="#" className="trcakticketlink">Track Tickets</a>
+      </div>
+
+      <div className="row supportContent  ">
+        <div className="col-6 md-6" id="leftRowDiv">
+          <h1>
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+
+          <input
+            type="text"
+            placeholder="Eg: how do I activate F&O, why is my order getting rejected"
+          />
+
+          <div className="quickLinks">
+            <a href="#">Track account opening</a>
+            <a href="#">Track segment activation</a>
+            <a href="#">Intraday margins</a>
+            <a href="#">Kite user manual</a>
+          </div>
+        </div>
+       <div className="col-1"></div>
+        <div className="col-5  md-6" id="RighttRowDiv">
+          <h1>Featured</h1>
+          <ol>
+            <li>
+              <a href="#">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="#">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+
+    </section>
+  );
 }
 
 export default Hero;

4bb7ef5c7b8805e7b8ee1a6d31a48aa421d3a794
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-01-23 07:39:34 +0530

    Add Signup and Login folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,28 +4,58 @@ import "./index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./landing_page/home/HomePage";
-import Signup from "./landing_page/signup/Signup";
+import Signup from "./landing_page/auth/Signup";
+import Login from "./landing_page/auth/Login";
 import ProductPage from "./landing_page/product/ProductPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import AboutPage from "./landing_page/about/AboutPage";
 import SupportPage from "./landing_page/support/SupportPage";
-import Navbar from './landing_page/Navbar';
-import Footer from './landing_page/Footer';
-import NotFound from './landing_page/NotFound';
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+import NotFound from "./landing_page/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//   <Navbar/>
+//     <Routes>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/signup" element={<Signup />} />
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/About" element={<AboutPage />} />
+//       <Route path="/Product" element={<ProductPage />} />
+//       <Route path="/Pricing" element={<PricingPage />} />
+//       <Route path="/Support" element={<SupportPage />} />
+//       <Route path="*" element={<NotFound />} />
+//     </Routes>
+//     <Footer/>
+//   </BrowserRouter>
+// );
 root.render(
   <BrowserRouter>
-  <Navbar/>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      {/* Pages WITHOUT navbar/footer */}
       <Route path="/signup" element={<Signup />} />
-      <Route path="/About" element={<AboutPage />} />
-      <Route path="/Product" element={<ProductPage />} />
-      <Route path="/Pricing" element={<PricingPage />} />
-      <Route path="/Support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Pages WITH navbar/footer */}
+      <Route
+        path="/*"
+        element={
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/product" element={<ProductPage />} />
+              <Route path="/pricing" element={<PricingPage />} />
+              <Route path="/support" element={<SupportPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </>
+        }
+      />
     </Routes>
-    <Footer/>
-  </BrowserRouter>
+  </BrowserRouter>,
 );

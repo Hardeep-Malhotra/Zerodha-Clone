baa2dd30b3e3ca6a2e1a2b07d90efbbae6adca63
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-02-10 21:57:31 +0530

    Modifeid :  UserMOdel like cheneg password logic

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -21,8 +21,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function () {
+// userSchema.pre("save", async function () {
+//   this.password = await bcrypt.hash(this.password, 12);
+// });
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // 🔥 important
   this.password = await bcrypt.hash(this.password, 12);
+ 
 });
+
 
 module.exports = mongoose.model("User", userSchema);
